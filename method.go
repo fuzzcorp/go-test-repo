462bb55baacbb9796892e3414ad0a30aec729856
Everest Munro-Zeisberger <evmunro1@gmail.com>
2020-09-09 15:57:21 -0700

    Add new bug

--- method.go
+++ method.go
@@ -3,6 +3,9 @@ package tutorial
 // BrokenMethod has a bug - it will try to read the 4th
 // index of Data even when it only has a length of 3.
 func BrokenMethod(Data string) bool {
+	if Data == "FUZZ" {
+		panic("we got sum fuzz")
+	}
 	return len(Data) >= 2 &&
 		Data[0] == 'F' &&
 		Data[1] == 'U' &&
