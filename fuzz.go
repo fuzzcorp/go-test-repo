682f148ee554f98f750ec16f39786f84efe0f7af
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2020-09-16 17:04:21 -0700

    Update fuzz.go

--- fuzz.go
+++ fuzz.go
@@ -2,7 +2,7 @@ package tutorial
 
 // FuzzerEntrypoint is the method Fuzzbuzz will repeatedly
 // run with new tests to try and find bugs in BrokenMethod
-// another small edit
+// another small
 func FuzzerEntrypoint(Data []byte) int {
 	// Convert Data to the type we need:
 	testString := string(Data)
